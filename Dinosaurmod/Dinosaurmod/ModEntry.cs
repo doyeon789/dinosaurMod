ce0ae6fed4dcb0d19ca5279a3e3e7b1f8f2f2061
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2025-03-29 20:58:11 +0900

    Add files via upload

---
+++ Dinosaurmod/Dinosaurmod/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ArchaeologyMod
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("고고학 모드가 실행되었습니다!", LogLevel.Info);
+        }
+    }
+}
